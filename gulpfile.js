e5c27404d6fc547002e996b0b461cdb8a8732423
Antonio Coppola <coppola.ant@gmail.com>
2016-06-26 19:02:34 +0100

    add uglify e minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,32 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var minifyCss = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+
+gulp.task('scripts', function(){
+  gulp.src(['src/scripts/main.js'])
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('styles', function(){
+  gulp.src(['src/styles/**/*.css'])
+    .pipe(sourcemaps.init())
+    .pipe(minifyCss())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(browserSync.stream());
+});
 
 gulp.task('default', function(){
-  console.log('Primo task');
+  browserSync.init({
+    server: './'
+  });
+  gulp.watch('src/styles/**/*.css', ['styles']);
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
+  gulp.watch('*.html', browserSync.reload);
 });
