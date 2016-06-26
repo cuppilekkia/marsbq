ab56ff2c4c2c836ba172a06fc55db50658d15c89
Antonio Coppola <coppola.ant@gmail.com>
2016-06-26 20:51:57 +0100

    js initial struct

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,24 @@
-console.log('HELLO WORLD');
-console.log('Hello Againnn');
+var model = {
+  init: function() {
+    console.log('model init');
+  }
+};
+
+var helper = {
+
+};
+
+var view = {
+  init: function () {
+    console.log('view init');
+  }
+};
+
+var app = {
+  init: function () {
+    model.init();
+    view.init();
+  }
+};
+
+app.init();
