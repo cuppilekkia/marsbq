393744f669d393c0deaf57268fc385a0d2b34437
Antonio Coppola <coppola.ant@gmail.com>
2016-06-26 21:34:47 +0100

    countdown

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,30 @@
+var countdown = require('countdown');
+
 var model = {
   init: function() {
     console.log('model init');
-  }
+  },
+
+  currentDate: new Date(),
+  eventDate: new Date(2056,10,05)
 };
 
 var helper = {
-
+  dateDiff: function () {
+    return countdown(model.currentDate, model.eventDate);
+  }
 };
 
 var view = {
   init: function () {
-    console.log('view init');
+    view.showCountdown();
+  },
+
+  showCountdown: function () {
+    var diff = helper.dateDiff();
+    document.querySelector('#years').innerHTML = diff.years;
+    document.querySelector('#months').innerHTML = diff.months;
+    document.querySelector('#days').innerHTML = diff.days;
   }
 };
 
